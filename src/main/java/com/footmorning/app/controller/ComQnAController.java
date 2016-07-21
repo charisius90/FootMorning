801db0c3219a308cdb0b8ffd68c1c93a910f96c4
Soohang Park <charisius90@gmail.com>
2016-07-21 21:14:32 +0900

    QNA게시판 기본 backend코드 작성

--- src/main/java/com/footmorning/app/controller/ComQnAController.java
+++ src/main/java/com/footmorning/app/controller/ComQnAController.java
@@ -1,8 +1,47 @@
 package com.footmorning.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.footmorning.app.service.ComQnAService;
+
+/**
+ * 커뮤니티 Q&A 게시판
+ * @author 박수항
+ */
 @Controller
+@RequestMapping("/com/qna/*")
 public class ComQnAController {
+	@Autowired
+	private ComQnAService service;
 
+	
+	@RequestMapping("comQnARegister")
+	public void registerGET(){}
+	@RequestMapping(value="comQnARegister", method=RequestMethod.POST)
+	public String registerPOST(){
+		return "redirect:/com/qna/comQnAListAll";
+	}
+	
+	@RequestMapping("comQnAListAll")
+	public void listAll() throws Exception{
+	}
+	
+	@RequestMapping("comQnARead")
+	public void readGET() throws Exception{
+	}
+	
+	@RequestMapping("comQnAUpdate")
+	public void updateGET() throws Exception{}
+	@RequestMapping(value="comQnAUpdate", method=RequestMethod.POST)
+	public String updatePOST() throws Exception{
+		return "/com/boast/comQnARead";
+	}
+	
+	@RequestMapping("comQnADelete")
+	public String deleteGET() throws Exception{
+		return "redirect:/com/boast/comQnAListAll";
+	}
 }
---
+++ src/main/java/com/footmorning/app/persistence/com/ComQnADAOImpl.java
@@ -0,0 +1,44 @@
+package com.footmorning.app.persistence.com;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.footmorning.app.domain.ComQnADTO;
+import com.footmorning.app.persistence.ComQnADAO;
+
+@Repository
+public class ComQnADAOImpl implements ComQnADAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private static final String NAMESPACE = "com.charisius.mappers.comQnAMapper";
+
+	@Override
+	public void register(ComQnADTO dto) throws Exception {
+		sqlSession.insert(NAMESPACE + ".register", dto);
+	}
+
+	@Override
+	public ComQnADTO read(Integer no) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + ".read", no);
+	}
+
+	@Override
+	public void update(ComQnADTO dto) throws Exception {
+		sqlSession.update(NAMESPACE + ".update", dto);
+	}
+
+	@Override
+	public void delete(Integer no) throws Exception {
+		sqlSession.delete(NAMESPACE + ".delete", no);
+	}
+
+	@Override
+	public List<ComQnADTO> listAll() throws Exception {
+		return sqlSession.selectList(NAMESPACE + ".listAll");
+	}
+
+}
