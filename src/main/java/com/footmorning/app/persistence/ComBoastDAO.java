3a8a2461620bb2bb1a3acd187d0b78a02bddd51a
Soohang Park <charisius90@gmail.com>
2016-07-21 17:42:35 +0900

    커뮤니티 컨트롤러 자유게시판까지 생성

---
+++ src/main/java/com/footmorning/app/persistence/ComBoastDAO.java
@@ -0,0 +1,11 @@
+package com.footmorning.app.persistence;
+
+import java.util.List;
+
+public interface ComBoastDAO {
+	public void create(ComBoastDTO dto) throws Exception;
+	public ComBoastDTO read(Integer bno) throws Exception;
+	public void update(ComBoastDTO vo) throws Exception;
+	public void delete(Integer bno) throws Exception;
+	public List<ComBoastDTO> listAll() throws Exception;
+}
