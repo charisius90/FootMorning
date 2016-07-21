4cd335e769eb3e9c35dce19ae006c39806666f55
Administrator <Administrator@WIN-9SRVGP2AQUK>
2016-07-21 17:43:22 +0900

    MemberController 생성

---
+++ src/main/java/com/footmorning/app/controller/MemberController.java
@@ -0,0 +1,56 @@
+package com.footmorning.app.controller;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.WebUtils;
+
+@Controller
+@RequestMapping("member/*")
+public class MemberController {
+	/*@Inject
+	private MemberService service;*/
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@RequestMapping("/memberLogin")
+	public void login(){
+	}
+
+	@RequestMapping(value="/memberLogin", method=RequestMethod.POST)
+	public String loginComplete() {
+		return "index";
+	}
+	
+	@RequestMapping("/memberLogout")
+	public String logout(HttpServletRequest req){
+		req.getSession().invalidate();
+		return "index";
+	}
+	
+	@RequestMapping("/memberSignUp")
+	public void signup(){
+	}
+	
+	@RequestMapping(value="/memberSignUp", method=RequestMethod.POST)
+	public String signupComplete(){
+		return "index";
+	}
+	
+	@RequestMapping("/memberSearchID")
+	public void searchID(){
+	}
+	
+	@RequestMapping("/memberSearchPW")
+	public void searchPW(){
+	}
+}
